55c17251d2725b282fa4506b7dcfde6eecbe9be9
Eden2108 <163290899+Eden2108@users.noreply.github.com>
2025-09-09 10:45:41 +0000

    Add project files.

---
+++ Controllers/AcademicManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSystem.Controllers
+{
+    public class AcademicManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProgrammeCoordinatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSystem.Controllers
+{
+    public class ProgrammeCoordinatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
